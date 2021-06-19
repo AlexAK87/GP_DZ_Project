6d084fd22db18b29e460d4d75d058665db722974
Alex_K <aleksey.kurochkin1987@gmail.com>
2021-06-19 09:23:48 +0600

    lesson 11 task 1,2,3

--- lesson_11/Kurochkin_Alexey_3.py
+++ lesson_11/Kurochkin_Alexey_3.py
@@ -5,6 +5,24 @@ class MyExceptions(Exception):
     def __init__(self, text):
         self.text = text
 
-    def my_fun(self):
-        return False
-        pass
+
+number_list = []
+
+
+def get_number(num):
+    try:
+        if type(num) == int():
+            number_list.append(num)
+        else:
+            print(number_list)
+            raise MyExceptions('Программа работает только с целыми цифрами')
+
+    except ValueError:
+        return "Error type of value!"
+    except MyExceptions as mr:
+        return mr
+    else:
+        return f'Результат: {result}'
+
+
+
